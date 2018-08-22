39da6656b8a372af3c907ff591715b67fd562240
Denis Karakoluichka <karakoluchka@kassir.ru>
2018-08-22 12:28:29 +0300

    improve tests

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringTest.java
@@ -1,11 +1,17 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.support.test.rule.ActivityTestRule;
 import android.util.Log;
+
+import junit.extensions.ActiveTestSuite;
 
 
 //@RunWith(AndroidJUnit4.class);
@@ -13,15 +19,21 @@ public class NonEmptyStringTest {
 
     private static final String LOG_TAG = "NonEmptyStringTest";
 
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+
+
     @SuppressWarnings("unchecked")
     @Test
     public void test() {
+
 
         // Testing that Async task successfully retrieves a non-empty string
         // You can test this from androidTest -> Run 'All Tests'
         Log.v("NonEmptyStringTest", "Running NonEmptyStringTest test");
         String result = null;
-        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(InstrumentationRegistry.getContext(), null);
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(mActivityRule.getActivity(), null);
         endpointsAsyncTask.execute();
         try {
             result = endpointsAsyncTask.get();
